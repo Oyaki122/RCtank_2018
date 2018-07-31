3930261b7220b7a91c472ce249a99eee84638b0b
Oyaki <oc.yaki122@gmail.com>
2018-07-31 15:20:22 +0900

    be able to controll

--- RCtank.ino
+++ RCtank.ino
@@ -1,5 +1,5 @@
 
-#define STICK_IDLE 10
+#define STICK_IDLE 5
 #define VRNG (410-deg)
 
 
@@ -7,12 +7,13 @@ bool cat_common;
 bool right_cat;
 bool left_cat;
 bool turret_roll;
-short speed_common;
-short speed_right;
-short speed_left;
-short speed_turret;
+float speed_common;
+float speed_right;
+float speed_left;
+float speed_turret;
 bool r_l;
 bool ultra_pivot_turn = false;
+bool turret_done = false;
 
 
 /*bool direction[4]
@@ -23,19 +24,37 @@ bool ultra_pivot_turn = false;
   */
 
 void stickcheck(double sticks[4]){
-  sticks[0] = sticks[0]*0.9+(analogRead(A0)-1023/2) * 0.1;
-  sticks[1] = sticks[1]*0.9+(analogRead(A1)-1023/2) * 0.1;
-  sticks[2] = sticks[0]*0.9+(analogRead(A2)-1023/2) * 0.1;
-  sticks[3] = sticks[1]*0.9+(analogRead(A3)-1023/2) * 0.1;
+ // sticks[0] = sticks[0]*0.9+(((int)pulseIn(7, HIGH,500000))-500) * 0.1;
+ // sticks[1] = sticks[1]*0.9+(((int)pulseIn(8, HIGH,500000))-500) * 0.1;
+ sticks[0] = ((int)pulseIn(A0,HIGH,50000))-500;
+ sticks[1] = ((int)pulseIn(A1,HIGH,50000))-500;
+  sticks[2] = sticks[2]*0.9+(analogRead(A2)-1023/2) * 0.1;
+  sticks[3] = sticks[3]*0.9+(analogRead(A3)-1023/2) * 0.1;
 
-  
+
+if(sticks[0]==-500){
+  if(analogRead(A0)>600){
+    sticks[0]=500;
+  }else{
+    sticks[0]=-500;
+  }
+}
+if(sticks[1]==-500){
+  if(analogRead(A1)>600){
+    sticks[1]=500;
+  }else{
+    sticks[1]=-500;
+  }
+}  
+
+
 
   char i;
   char deg;
   for(i=0;i<=3;i++){
     deg = (sticks[i]>0)? 1 : -1;
     sticks[i] = (abs(sticks[i])<STICK_IDLE) ? 0 : sticks[i]-(STICK_IDLE*deg);
-    sticks[i] = constrain(sticks[i], -400, 400);
+    sticks[i] = constrain(sticks[i]*0.8, -400, 400);
   }
   
 }
@@ -44,7 +63,10 @@ void stickcheck(double sticks[4]){
 void decider(){
   double sticks[4];
   stickcheck(sticks);
-  
+    
+Serial.print(sticks[0]);
+Serial.print(':');
+Serial.println(sticks[1]);
 
   double level;
   if(sticks[1]<0){
@@ -57,8 +79,9 @@ void decider(){
 
   
 
-  if(ultra_pivot_turn){
-    speed_left , speed_right = 255*(-1*level+1);
+  if(ultra_pivot_turn == true){
+    speed_left = 255*(-1*level+1);
+    speed_right = 255*(-1*level+1);
     if(r_l){
       left_cat = false;
       right_cat = true;
@@ -66,6 +89,7 @@ void decider(){
       left_cat = true;
       right_cat = false;
     }    
+
   }else{
 
 
@@ -99,6 +123,11 @@ void decider(){
     right_cat = cat_common; 
     left_cat = cat_common;//左右のキャタピラの方向を確定
 
+  //    Serial.print(speed_left);
+  // Serial.print(':');
+  // Serial.println(speed_right);
+
+
   }
 
   speed_turret= sticks[3]*51/80; //Rstick holizontalから基準速度を確定
@@ -108,52 +137,62 @@ void decider(){
     }else{
       turret_roll = true;
     }
-
-  Serial.print(speed_left);
-  Serial.print(" : ");
-  Serial.println(speed_right);
 }
 
 void send(){
-  analogWrite(9,speed_left);
-  analogWrite(10,speed_right);
-  analogWrite(11,speed_turret);
+  analogWrite(9,(unsigned int)speed_left);
+  analogWrite(10,(unsigned int)speed_right);
+  analogWrite(11,(unsigned int)speed_turret);
 
+/*
   int digitalPinValue[3];
-  digitalPinValue[0] = (left_cat)? HIGH:LOW;
-  digitalPinValue[1] = (right_cat)? HIGH:LOW;
+  digitalPinValue[0] = (left_cat==true)? HIGH:LOW;
+  digitalPinValue[1] = (right_cat==true)? HIGH:LOW;
   //digitalPinValue[2] = (turret_roll)? HIGH:LOW;
-
-  digitalWrite(3,digitalPinValue[0]);
-  digitalWrite(4,digitalPinValue[1]);
-  digitalWrite(5,digitalPinValue[2]);
-
-}
+*/
+  digitalWrite(3,left_cat);
+  digitalWrite(4,!left_cat);
+  digitalWrite(5,right_cat);
+  digitalWrite(6,!right_cat);
+  //digitalWrite(5,digitalPinValue[2]);
+  
+ }
 
 void setup()
 {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
   for (int i = 3; i <= 10; i++){
     pinMode(i, OUTPUT);
   }
+  // digitalWrite(7,LOW);
+  // pinMode(A1,OUTPUT);
+  // digitalWrite(A1,HIGH);
+
+  digitalWrite(7,HIGH);
+  // delay(100);
+  // digitalWrite(A1,LOW);
+  
   pinMode(11,INPUT);
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
   pinMode(A3, INPUT);
   pinMode(A4, INPUT);
-  analogReference(EXTERNAL);
+  Serial.begin(9600);
+  Serial.println("serialstart");
+
+ 
 }
 
 void loop()
 {
-  int hit = analogRead(A4);
+  /*int hit = analogRead(A4);
   if(hit>100){
     waiter();
   }
-  if(digitalRead(12)==HIGH){
-    digitalWrite(9,HIGH);
+
+  int turret_button = digitalRead(12);
+  if(turret_button==HIGH && turret_done==false){
+    digitalWrite(7,HIGH);
     if(analogRead(A5)>=500){
       digitalWrite(3,LOW);
       digitalWrite(4,LOW);
@@ -164,19 +203,24 @@ void loop()
     analogWrite(9,255);
     analogWrite(10,255);
 
-    delay(50);
+    delay(100);
 
     digitalWrite(9,LOW);
     analogWrite(9,0);
     analogWrite(10,0);
+    turret_done = true;
   }
-  ultra_pivot_turn = (digitalRead(13)==HIGH)? true:false;
+  if(turret_button==LOW)turret_done=false;
+
+  int pivot = digitalRead(13);
+  ultra_pivot_turn = (pivot==HIGH)? true:false;*/
   decider();
   send();
+  
 }
 
 void waiter(){  //waiterを起動すると無限ループで待機し、リセットボタンを押すことでリセットする
-  digitalWrite(10,HIGH);
+  digitalWrite(8,HIGH);
   while(true){
     if(digitalRead(11)==HIGH){
       asm volatile ("  jmp 0"); 

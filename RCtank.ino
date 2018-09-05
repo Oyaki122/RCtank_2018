eec4ccad67a18eb2cba49da5ed7c4b6c11d05444
Oyaki <oc.yaki122@gmail.com>
2018-09-05 13:20:50 +0900

    a little

--- RCtank.ino
+++ RCtank.ino
@@ -31,34 +31,25 @@ void stickcheck(double sticks[4]){
 
 
 
-<<<<<<< HEAD
-  if(sticks[0]==-500){
-    if(analogRead(A0)>600){
-      sticks[0]=500;
-    }else{
-      sticks[0]=-500;
-    }
-=======
 if(sticks[0]==-500){
   if(analogRead(A0)>600){
-    sticks[0]=500;
+	sticks[0]=500;
   }else{
-    sticks[0]=-500;
+	sticks[0]=-500;
   }
 }
 if(sticks[1]　==　-500){
   if(analogRead(A1)>600){
-    sticks[1]=500;
+	sticks[1]=500;
   }else{
-    sticks[1]=-500;
->>>>>>> dual-pins
+	sticks[1]=-500;
   }
   if(sticks[1]　==　-500){
-    if(analogRead(A1)>600){
-      sticks[1]=500;
-    }else{
-      sticks[1]=-500;
-    }
+	if(analogRead(A1)>600){
+	  sticks[1]=500;
+	}else{
+	  sticks[1]=-500;
+	}
   }  
 
 
@@ -66,9 +57,9 @@ if(sticks[1]　==　-500){
   char i;
   char deg;
   for(i=0;i<=3;i++){
-    deg = (sticks[i]>0)? 1 : -1;
-    sticks[i] = (abs(sticks[i])<STICK_IDLE) ? 0 : sticks[i]-(STICK_IDLE*deg);
-    sticks[i] = constrain(sticks[i], -400, 400);
+	deg = (sticks[i]>0)? 1 : -1;
+	sticks[i] = (abs(sticks[i])<STICK_IDLE) ? 0 : sticks[i]-(STICK_IDLE*deg);
+	sticks[i] = constrain(sticks[i], -400, 400);
   }
   
 }
@@ -77,65 +68,65 @@ if(sticks[1]　==　-500){
 void decider(){
   double sticks[4];
   stickcheck(sticks);
-    
+	
 Serial.print(sticks[0]);
 Serial.print(':');
 Serial.println(sticks[1]);
 
   double level;
   if(sticks[1]<0){
-    level = sticks[1]*1/400 +1;
-    r_l = true;  //左折がtrue
+	level = sticks[1]*1/400 +1;
+	r_l = true;  //左折がtrue
   } else{
-    level = -1* sticks[1]* 1/400 +1;
-    r_l = false;
+	level = -1* sticks[1]* 1/400 +1;
+	r_l = false;
   }
 
   
 
   if(ultra_pivot_turn == true){
-    speed_left = 255*(-1*level+1);
-    speed_right = 255*(-1*level+1);
-    if(r_l){
-      left_cat = false;
-      right_cat = true;
-    }else{
-      left_cat = true;
-      right_cat = false;
-    }    
+	speed_left = 255*(-1*level+1);
+	speed_right = 255*(-1*level+1);
+	if(r_l){
+	  left_cat = false;
+	  right_cat = true;
+	}else{
+	  left_cat = true;
+	  right_cat = false;
+	}    
 
   }else{
 
 
-    speed_common = sticks[0]*51/80; //Lstick verticalから基準速度を確定
-    if(speed_common<0){
-      speed_common *= -1;
-      cat_common = false;
-    }else{
-      cat_common = true;
-    }
+	speed_common = sticks[0]*51/80; //Lstick verticalから基準速度を確定
+	if(speed_common<0){
+	  speed_common *= -1;
+	  cat_common = false;
+	}else{
+	  cat_common = true;
+	}
 
-    
-    /* double level;  //旋回の基準数値を算出
-      bool r_l;
-      if(sticks[1]<0){
-        level = sticks[1]*1/400 +1;
-        r_l = true;  //左折がtrue
-      } else{
-        level = -1* sticks[1]* 1/400 +1;
-        r_l = false;
-      }*/
+	
+	/* double level;  //旋回の基準数値を算出
+	  bool r_l;
+	  if(sticks[1]<0){
+		level = sticks[1]*1/400 +1;
+		r_l = true;  //左折がtrue
+	  } else{
+		level = -1* sticks[1]* 1/400 +1;
+		r_l = false;
+	  }*/
 
-    if(r_l){  //左右別に分けて左右のキャタピタの速度を確定
-      speed_left = (level<0.2) ? 0 : speed_common * (1.25*level-0.25);
-      speed_right = speed_common;
-    }else{
-      speed_left = speed_common;
-      speed_right = (level<0.2) ? 0 : speed_common * (1.25*level-0.25);
-    }
+	if(r_l){  //左右別に分けて左右のキャタピタの速度を確定
+	  speed_left = (level<0.2) ? 0 : speed_common * (1.25*level-0.25);
+	  speed_right = speed_common;
+	}else{
+	  speed_left = speed_common;
+	  speed_right = (level<0.2) ? 0 : speed_common * (1.25*level-0.25);
+	}
 
-    right_cat = cat_common; 
-    left_cat = cat_common;//左右のキャタピラの方向を確定
+	right_cat = cat_common; 
+	left_cat = cat_common;//左右のキャタピラの方向を確定
 
   //    Serial.print(speed_left);
   // Serial.print(':');
@@ -145,12 +136,12 @@ Serial.println(sticks[1]);
   }
 
   speed_turret= sticks[3]*51/80; //Rstick holizontalから基準速度を確定
-    if(speed_turret<0){
-      speed_turret *= -1;
-      turret_roll = false;
-    }else{
-      turret_roll = true;
-    }
+	if(speed_turret<0){
+	  speed_turret *= -1;
+	  turret_roll = false;
+	}else{
+	  turret_roll = true;
+	}
 }
 
 void send(){
@@ -175,7 +166,7 @@ void send(){
 void setup()
 {
   for (int i = 3; i <= 10; i++){
-    pinMode(i, OUTPUT);
+	pinMode(i, OUTPUT);
   }
   // digitalWrite(7,LOW);
   // pinMode(A1,OUTPUT);
@@ -201,28 +192,28 @@ void loop()
 {
   /*int hit = analogRead(A4);
   if(hit>100){
-    waiter();
+	waiter();
   }
 
   int turret_button = digitalRead(12);
   if(turret_button==HIGH && turret_done==false){
-    digitalWrite(7,HIGH);
-    if(analogRead(A5)>=500){
-      digitalWrite(3,LOW);
-      digitalWrite(4,LOW);
-    }else{
-      digitalWrite(3,HIGH);
-      digitalWrite(4,HIGH);
-    }
-    analogWrite(9,255);
-    analogWrite(10,255);
+	digitalWrite(7,HIGH);
+	if(analogRead(A5)>=500){
+	  digitalWrite(3,LOW);
+	  digitalWrite(4,LOW);
+	}else{
+	  digitalWrite(3,HIGH);
+	  digitalWrite(4,HIGH);
+	}
+	analogWrite(9,255);
+	analogWrite(10,255);
 
-    delay(100);
+	delay(100);
 
-    digitalWrite(9,LOW);
-    analogWrite(9,0);
-    analogWrite(10,0);
-    turret_done = true;
+	digitalWrite(9,LOW);
+	analogWrite(9,0);
+	analogWrite(10,0);
+	turret_done = true;
   }
   if(turret_button==LOW)turret_done=false;
 
@@ -236,8 +227,8 @@ void loop()
 void waiter(){  //waiterを起動すると無限ループで待機し、リセットボタンを押すことでリセットする
   digitalWrite(8,HIGH);
   while(true){
-    if(digitalRead(11)==HIGH){
-      asm volatile ("  jmp 0"); 
-    }
+	if(digitalRead(11)==HIGH){
+	  asm volatile ("  jmp 0"); 
+	}
   }
 }
